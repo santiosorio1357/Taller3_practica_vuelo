8c6004b02293e09ff669ca1b48c075ff0aa6c363
Santiago <santiosorio1357@gmail.com>
2022-10-14 21:07:50 -0500

    Control de sillas

---
+++ js/controlSillas.js
@@ -0,0 +1,29 @@
+function bloquearAsiento(idAsiento) {
+    const asiento = document.getElementById(idAsiento);
+    console.log(asiento)
+    asiento.setAttribute('disabled', '');
+}
+
+
+var boton = document.getElementsByTagName('input');
+console.log(boton)
+
+let sillasEscogidas = [];
+
+
+function seleccionMaxima(objeto){
+    if(sillasEscogidas.indexOf(objeto.id) == -1){
+        if(sillasEscogidas.length < 3){
+            sillasEscogidas.push(objeto.id)
+        }
+        else{
+            objeto.checked = false;
+            console.log('No puedes escoger más sillas.')
+        }
+    }
+    else{
+        sillasEscogidas.splice(sillasEscogidas.indexOf(objeto.id),1)
+    }
+
+    console.log(sillasEscogidas)
+}
